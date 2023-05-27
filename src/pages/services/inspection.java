a4ac2c9f0b26f7695b5eee06d7ab47450087edea
unknown <g.munkhorgil06@gmail.com>
2023-05-27 17:17:02 +0800

    services

--- src/pages/services/inspection.java
+++ src/pages/services/inspection.java
@@ -1,27 +1,78 @@
 package pages.services;
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Dimension;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-import javax.swing.BoxLayout;
-import javax.swing.JPanel;
+public class inspection extends JPanel implements ActionListener{
+    public static JButton selectedButton;
+    public static JButton homeBtn;
+    public static JButton serviceBtn;
+    public static JButton infoBtn;
+    public inspection() {
+        setLayout(new BorderLayout());
 
-public class inspection extends JPanel{
-    
-    public inspection(){
-        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-        JPanel aboutUs = new JPanel();
-        aboutUs.setBackground(Color.green);
-        aboutUs.setPreferredSize(new Dimension(getWidth(), 500));
-        JPanel doctors = new JPanel();
-        doctors.setBackground(Color.red);
-        doctors.setPreferredSize(new Dimension(getWidth(), 500));
-        JPanel newInformation = new JPanel();
-        newInformation.setBackground(Color.orange);
-        newInformation.setPreferredSize(new Dimension(getWidth(), 500));
+        
+        setPreferredSize(new Dimension(300, HEIGHT));
+        setBackground(Color.GRAY);
+        setBorder(BorderFactory.createEmptyBorder(10, 0, 00, 0));
+        
+        JPanel menuJPanel = new JPanel();
+        menuJPanel.setLayout(new GridLayout(3, 1));
+        menuJPanel.setPreferredSize(new Dimension(200, getHeight()));
+        menuJPanel.setBackground(Color.GRAY);
+        
+        homeBtn = createStyledButton("Нүүр хуудас");
+        homeBtn.setName("homeBtn");
+        serviceBtn = createStyledButton("Үйлчилгээ");
+        serviceBtn.setName("serviceBtn");
+        infoBtn = createStyledButton("Мэдээлэл");
+        infoBtn.setName("infoBtn");
 
-        add(aboutUs, BorderLayout.NORTH);
-        add(doctors, BorderLayout.NORTH);
-        add(newInformation, BorderLayout.NORTH);
+        menuJPanel.add(homeBtn);
+        menuJPanel.add(serviceBtn);
+        menuJPanel.add(infoBtn);
+
+        
+        add(menuJPanel, BorderLayout.EAST);
+       
+
+        selectedButton=homeBtn;
+        homeBtn.setBackground(Color.blue);
+        
+
+
+        
+    }
+
+    private JButton createStyledButton(String text) {
+        JButton button = new JButton(text);
+        button.setBackground(Color.GRAY);
+        button.setForeground(Color.WHITE);
+        button.setFocusPainted(false);
+        button.addActionListener(this);
+        button.setPreferredSize(new Dimension(getWidth(), 100));
+        return button;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // Get the source button that was clicked
+        JButton source = (JButton) e.getSource();
+        String name=source.getName();
+        if(selectedButton.getName()!=name){
+            selectedButton.setBackground(Color.GRAY);
+            selectedButton=source;
+            source.setBackground(Color.blue);
+        }
+        
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                new inspection();
+            }
+        });
     }
 }
