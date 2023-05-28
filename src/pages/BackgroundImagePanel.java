56178a8d7bc362b71535a1f5865e0ebfb9253971
Orqil <g.munkhorgil06@gmail.com>
2023-05-28 23:32:21 +0800

    d

---
+++ src/pages/BackgroundImagePanel.java
@@ -0,0 +1,10 @@
+package pages;
+
+import java.awt.Image;
+
+public class BackgroundImagePanel {
+
+    public BackgroundImagePanel(Image image) {
+    }
+
+}
--- src/pages/information.java
+++ src/pages/information.java
@@ -3,26 +3,50 @@ package pages;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
 import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class information extends JPanel{
+    private JLabel label;
+    private JButton testResult;
+    private JButton timeOrder;
+    private BufferedImage backgroundImage;
     public information(){
         setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-        JPanel aboutUs = new JPanel();
-        aboutUs.setBackground(Color.green);
-        aboutUs.setPreferredSize(new Dimension(getWidth(), 500));
-        JPanel doctors = new JPanel();
-        doctors.setBackground(Color.red);
-        doctors.setPreferredSize(new Dimension(getWidth(), 500));
-        JPanel newInformation = new JPanel();
-        newInformation.setBackground(Color.orange);
-        newInformation.setPreferredSize(new Dimension(getWidth(), 500));
+        
+        ImageIcon icon = new ImageIcon(getClass().getResource("..\\asset\\images\\Fam1.jpg")) ;
+        Image originalImage=icon.getImage().getScaledInstance(1250, 900, Image.SCALE_SMOOTH);
+        
+        JLabel iconLabel = new JLabel(new ImageIcon(originalImage));
 
-        add(aboutUs, BorderLayout.NORTH);
-        add(doctors, BorderLayout.NORTH);
-        add(newInformation, BorderLayout.NORTH);
-
+        iconLabel.setPreferredSize(new Dimension(1250, 900));
+        this.add(iconLabel, BorderLayout.CENTER);
+        try {
+            backgroundImage = ImageIO.read(getClass().getResource("../asset/images/Map.png")); 
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
     }
+    @Override
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            if (backgroundImage != null) {
+                int width = getWidth();
+                int height = (int) ((double) width / backgroundImage.getWidth() * backgroundImage.getHeight());
+                g.drawImage(backgroundImage, 0, 0, width, height, this);
+                
+            }
+        }
+
 }
+
